8ffac24ccdd75440c2c2ca6ccb725714e7208f35
Halit Akkuş <46889995+halitakkus@users.noreply.github.com>
2021-10-22 15:13:04 +0300

    -

--- IdentityServer/IdentityServer.Client1/Controllers/HomeController.cs
+++ IdentityServer/IdentityServer.Client1/Controllers/HomeController.cs
@@ -1,37 +1,66 @@
-﻿using IdentityServer.Client1.Models;
+﻿using IdentityModel.Client;
+using IdentityServer.Client1.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace IdentityServer.Client1.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IConfiguration configuration)
         {
-            _logger = logger;
+            _configuration = configuration;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            HttpClient httpClient = new HttpClient();
+
+            var disco = await httpClient.GetDiscoveryDocumentAsync("https://localhost:5001");
+
+            if (disco.IsError)
+            {
+                //LOG
+            }
+
+            var clientCredentialsTokenRequest = new ClientCredentialsTokenRequest();
+
+            clientCredentialsTokenRequest.ClientId = _configuration["Client:ClientId"];
+            clientCredentialsTokenRequest.ClientSecret = _configuration["Client:ClientSecret"];
+            clientCredentialsTokenRequest.Address = disco.TokenEndpoint;
+
+            var token = await httpClient.RequestClientCredentialsTokenAsync(clientCredentialsTokenRequest);
+
+            if (token.IsError)
+            {
+                //LOG
+            } 
+
+            //https://localhost:5006
+
+            httpClient.SetBearerToken(token.AccessToken);
+
+            var response = await httpClient.GetAsync("https://localhost:6001/api/product/getproducts");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+            }
+            else
+            {
+                //LOG
+            }
+
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
---
+++ IdentityServer/IdentityServer.Client1/Controllers/ProductContoller.cs
@@ -0,0 +1,16 @@
+﻿using IdentityModel.Client;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace IdentityServer.Client1.Controllers
+{
+    public class ProductContoller : Controller
+    {
+        
+    }
+}
