358d1e1152f1f6d0c4c944e1057f641f2fa1a4f7
Halit Akkuş <46889995+halitakkus@users.noreply.github.com>
2021-11-01 16:10:49 +0300

    Sign out işlemleri yapıldı.

--- IdentityServer/IdentityServer.Client1/Controllers/UserController.cs
+++ IdentityServer/IdentityServer.Client1/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -12,10 +13,13 @@ namespace IdentityServer.Client1.Controllers
     {
         public IActionResult Index()
         {
-
-
-
             return View();
+        }
+
+        public async Task LogOut()
+        {
+            await HttpContext.SignOutAsync("Cookies"); //IdentityServer4 üzerinden çıkış yapar.
+            await HttpContext.SignOutAsync("oidc"); //Kendi uygulamamızdan çıkış yapar.
         }
     }
 }
