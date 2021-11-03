287cbe40c8a5edc773cf2e865c7a6d0ae9cd1252
Halit Akkuş <46889995+halitakkus@users.noreply.github.com>
2021-11-03 15:58:54 +0300

    custom üyelik işlemi yapıldı

---
+++ IdentityServer/IdentityServer.AuthServer/Migrations/20211103125635_Initial.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IdentityServer.AuthServer.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CustomUsers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomUsers", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "CustomUsers",
+                columns: new[] { "Id", "City", "Email", "Password", "UserName" },
+                values: new object[] { 1, null, "halitakkus03@gmail.com", "password", null });
+
+            migrationBuilder.InsertData(
+                table: "CustomUsers",
+                columns: new[] { "Id", "City", "Email", "Password", "UserName" },
+                values: new object[] { 2, null, "test@gmail.com", "password", null });
+
+            migrationBuilder.InsertData(
+                table: "CustomUsers",
+                columns: new[] { "Id", "City", "Email", "Password", "UserName" },
+                values: new object[] { 3, null, "test2@gmail.com", "password", null });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CustomUsers");
+        }
+    }
+}
---
+++ IdentityServer/IdentityServer.AuthServer/Models/CustomDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer.AuthServer.Models
+{
+    public class CustomDbContext : DbContext
+    {
+        public CustomDbContext(DbContextOptions opts) : base(opts)
+        {
+
+        }
+
+        public DbSet<CustomUser> CustomUsers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CustomUser>().HasData(
+           new CustomUser
+            {
+                Id = 1,
+                Email = "halitakkus03@gmail.com",
+                Password = "password"
+            },
+           new CustomUser
+           {
+               Id = 2,
+               Email = "test@gmail.com",
+               Password = "password"
+           },
+           new CustomUser
+           {
+               Id = 3,
+               Email = "test2@gmail.com",
+               Password = "password"
+           }
+           );
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
