6159ee9cabca8d5eaf4bfab1af847465687d1201
Halit Akkuş <46889995+halitakkus@users.noreply.github.com>
2021-11-03 16:25:12 +0300

    Identity server için repository tanımlandı

---
+++ IdentityServer/IdentityServer.AuthServer/Services/ICustomUserRepository.cs
@@ -0,0 +1,32 @@
+﻿using IdentityServer.AuthServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer.AuthServer.Services
+{
+    internal interface ICustomUserRepository
+    {
+        /// <summary>
+        /// Email ve şifre doğrulama işlemi.
+        /// </summary>
+        /// <param name="email"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        Task<bool> Validate(string email, string password);
+
+        /// <summary>
+        /// İd değerine göre kullanıcı bul.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<CustomUser> FindById(int id);
+        /// <summary>
+        /// Email parametresine göre kullanıcı bul.
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns></returns>
+        Task<CustomUser> FindByEmail(string email);
+    }
+}
